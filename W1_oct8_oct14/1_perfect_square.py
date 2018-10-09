9f3a6b225d38f2d964e7761b48fb16b10043232b
Alexis LE CONTE <32488985+AlexisLeConte@users.noreply.github.com>
2018-10-09 18:49:34 +0200

    Add solution for problem 1 of week 1.

---
+++ W1_oct8_oct14/1_perfect_square.py
@@ -0,0 +1,19 @@
+class Solution(object):
+   def isPerfectSquare(self, num):
+       """
+       :type num: int
+       :rtype: bool
+       """
+       i=1
+       r=0
+       mini=1
+       maxi=num
+       while(i!=r):
+           if(i**2==num) : return True
+           if(i**2<num) :
+               mini=i
+           else:
+               maxi=i
+           r=i
+           i=int((maxi+mini)/2)
+       return False
