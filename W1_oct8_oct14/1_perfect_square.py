d894abd3ac9dba589fecba5bdcecd3401f7c71a8
Alexis LE CONTE <32488985+AlexisLeConte@users.noreply.github.com>
2018-10-09 18:55:26 +0200

    Refactored solution for problem 1 of week 1.

--- W1_oct8_oct14/1_perfect_square.py
+++ W1_oct8_oct14/1_perfect_square.py
@@ -4,16 +4,18 @@ class Solution(object):
        :type num: int
        :rtype: bool
        """
-       i=1
-       r=0
-       mini=1
-       maxi=num
-       while(i!=r):
-           if(i**2==num) : return True
-           if(i**2<num) :
-               mini=i
+       i = 1
+       r = 0
+       mini = 1
+       maxi = num
+       while(i != r):
+           if(i**2 == num):
+               return True
+           if(i**2 < num):
+               mini = i
            else:
-               maxi=i
-           r=i
-           i=int((maxi+mini)/2)
+               maxi = i
+           r = i
+           i = int((maxi + mini) / 2)
        return False
+   
