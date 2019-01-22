2eb26e9a6943ac42c6a6a9cfe741fc00eb652060
Louis Sugy <louis.sugy@gmail.com>
2019-01-22 23:55:26 +0100

    Now the solution is truly O(n) (a little longer though)

--- W13_jan21_jan27/teads_contest.py
+++ W13_jan21_jan27/teads_contest.py
@@ -11,22 +11,30 @@ def main():
         graph[i].append(j)
         graph[j].append(i)
 
-    seen = set()
-    _, max_path = tree_descent_rec(graph, seen, list(graph.keys())[0])
+    _, max_path = tree_descent_rec(graph, None, list(graph.keys())[0])
     print(int(ceil(max_path / 2)))
 
 
-def tree_descent_rec(graph, seen, node):
-    seen.add(node)
+def tree_descent_rec(graph, parent, node):
     child_res = []
     for child in graph[node]:
-        if child not in seen:
-            child_res.append(tree_descent_rec(graph, seen, child))
+        if child != parent:
+            child_res.append(tree_descent_rec(graph, node, child))
     if not child_res:
         return (1, 0)
     depth = max(res[0] for res in child_res) + 1
-    max_incl = (sum(sorted((res[0] for res in child_res), reverse=True)[:2])
-                if len(child_res) > 1 else 0)
+    if len(child_res) > 1:
+        max1 = max(child_res[0][0], child_res[1][0])
+        max2 = min(child_res[0][0], child_res[1][0])
+        for res in child_res[2:]:
+            if res[0] > max1:
+                max2 = max1
+                max1 = res[0]
+            elif res[0] > max2:
+                max2 = res[0]
+        max_incl = max1 + max2
+    else:
+        max_incl = 0
     max_path = max(max(res[1] for res in child_res), max_incl, depth)
     return (depth, max_path)
 
